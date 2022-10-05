9c4a065c4f9d7170cae6d1c9ae29b4e2efe62fb2
Harlan Sim <harlansim@gmail.com>
2022-10-04 17:49:00 -0700

    clean up parse functions

--- src/api/parse.ts
+++ src/api/parse.ts
@@ -2,21 +2,22 @@ import { DAYS, FORECAST_LENGTH } from '../utils/constants';
 import { WeatherData, WeatherResponse } from '../utils/types';
 
 export const parseWeatherData = (data: WeatherResponse): WeatherData[] => {
-  let result = [];
+  let result: WeatherData[] = [];
   let { current, daily, timezone } = data;
   if (data && current && daily) {
     result.push({
       icon: current.weather[0]?.icon ?? '',
-      temp: current.temp ? Math.round(current.temp - 273.15) : 0,
+      temp: convertFromKelvinToCelsuis(current.temp),
       day: 'Today',
       description: current.weather[0]?.main,
     });
     for (let index = 0; index < FORECAST_LENGTH; index++) {
       const day = daily[index];
       result.push({
-        icon: day?.weather[0]?.icon,
-        temp: day?.temp ? Math.round(day?.temp?.day - 273.15) : 0,
+        icon: day?.weather[0]?.icon ?? '',
+        temp: convertFromKelvinToCelsuis(day?.temp?.day),
         day: getDayFromTimeZone(timezone, index + 1),
+        description: '',
       });
     }
   }
@@ -31,4 +32,8 @@ const getDayFromTimeZone = (timezone: string, increment: number): string => {
   // Get the day index, then mod it by 7 to get day from DAYS
   const dayIndex: number = new Date(dateString).getDay() + increment;
   return DAYS[dayIndex % 7];
+};
+
+const convertFromKelvinToCelsuis = (temp: number): number => {
+  return temp ? Math.round(temp - 273.15) : 0;
 };
