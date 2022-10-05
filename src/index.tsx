3b62734d3e0fede6bc1688f1906c1d3ad3df0713
Harlan Sim <harlansim@gmail.com>
2022-10-04 17:00:28 -0700

    breakup styles in multiple files

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './components/home';
-import './styles/styles.less';
+import './styles/home';
+import './styles/header';
+import './styles/weather';
+import './styles/icon';
 import '@fontsource/teko';
 import '@fontsource/montserrat';
 
